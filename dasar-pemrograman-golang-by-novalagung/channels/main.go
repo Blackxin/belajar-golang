ac61b30eccde648e6e41665ee02c0b30202c970f
tfkhdyt <tfkhdyt@pm.me>
2022-06-12 10:42:15 +0700

    Add "buffered channel"

--- dasar-pemrograman-golang-by-novalagung/channels/main.go
+++ dasar-pemrograman-golang-by-novalagung/channels/main.go
@@ -5,15 +5,15 @@ import (
 	"runtime"
 )
 
-func printMessage(what chan string) {
-	fmt.Println(<-what)
-}
+// func printMessage(what chan string) {
+// 	fmt.Println(<-what)
+// }
 
 func main() {
 	// penerapan channel
-	runtime.GOMAXPROCS(2)
+	// runtime.GOMAXPROCS(2)
 
-	var messages = make(chan string)
+	// var messages = make(chan string)
 
 	// var sayHelloTo = func(who string) {
 	// 	var data = fmt.Sprintf("hello %s", who)
@@ -34,14 +34,29 @@ func main() {
 	// fmt.Println(message3)
 
 	// channel sebagai tipe data parameter
-	for _, each := range []string{"wick", "hunt", "bourne"} {
-		go func(who string) {
-			data := fmt.Sprintf("hello ke %s", who)
-			messages <- data
-		}(each)
-	}
+	// for _, each := range []string{"wick", "hunt", "bourne"} {
+	// 	go func(who string) {
+	// 		data := fmt.Sprintf("hello ke %s", who)
+	// 		messages <- data
+	// 	}(each)
+	// }
 
-	for i := 0; i < 3; i++ {
-		printMessage(messages)
+	// for i := 0; i < 3; i++ {
+	// 	printMessage(messages)
+	// }
+
+	// penerapan buffered channel
+	runtime.GOMAXPROCS(2)
+	messages := make(chan int, 3)
+	go func() {
+		for {
+			i := <-messages
+			fmt.Println("receive data:", i)
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("send data:", i)
+		messages <- i
 	}
 }
