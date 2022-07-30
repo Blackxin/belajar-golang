e30f7e92e12d2d8dc092d1c50dd131c88c6ee16a
tfkhdyt <tfkhdyt@pm.me>
2022-07-30 08:25:51 +0700

    ✨ feat(dasar pemrograman): add time duration

---
+++ dasar-pemrograman-golang-by-novalagung/time-duration/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// A.42.1. Praktek
+	// start := time.Now()
+	// time.Sleep(5 * time.Second)
+	// duration := time.Since(start)
+
+	// fmt.Println("time elapsed in seconds:", duration.Seconds())
+	// fmt.Println("time elapsed in minutes:", duration.Minutes())
+	// fmt.Println("time elapsed in hours:", duration.Hours())
+
+	// A.42.4. Kalkulasi Durasi Antara 2 Objek Waktu
+	t1 := time.Now()
+	time.Sleep(5 * time.Second)
+	t2 := time.Now()
+	duration := t2.Sub(t1)
+
+	fmt.Println("time elapsed in seconds:", duration.Seconds())
+	fmt.Println("time elapsed in minutes:", duration.Minutes())
+	fmt.Println("time elapsed in hours:", duration.Hours())
+}
